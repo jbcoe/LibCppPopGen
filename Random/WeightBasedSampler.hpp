e39fb63f6012b2594ee95bf443a74463d67303d6
Jonathan Coe <jon@jbcoe.net>
2011-12-04 13:23:55 +0000

    Added exceptions

--- Random/WeightBasedSampler.hpp
+++ Random/WeightBasedSampler.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <random>
+#include <stdexcept>
 
 template <typename Value_t>
 class WeightBasedSampler
@@ -12,7 +13,9 @@ class WeightBasedSampler
 		}
 
 		void add(double weight, Value_t value)
-		{       
+		{ 
+			if ( weight < 0.0 )
+				throw std::runtime_error("Weight in weight-based sampler must not be negative");
 			m_weightSum += weight;
 			m_cumulativeWeights.push_back(m_weightSum);
 			m_values.push_back(value);
@@ -36,7 +39,10 @@ class WeightBasedSampler
 			Value_t GetValue(RandomEngine_t& randomEngine)
 			{
 				double randomValue = m_uniformDist(randomEngine);
-				return m_values[getIndex(randomValue, m_cumulativeWeights)];
+        int valueIndex = getIndex(randomValue, m_cumulativeWeights);
+        if ( valueIndex < 0 )
+					throw std::runtime_error("Weight value cannot be found in cumulative weights");
+				return m_values[valueIndex];
 			}
 
 		/// return -1 if index cannot be found
