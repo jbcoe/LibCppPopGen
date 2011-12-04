2969cfd338a3ae902a5ecd3949dcf492ee0e08d1
Jonathan Coe <jon@jbcoe.net>
2011-12-04 13:17:39 +0000

    Renamed methods

--- Random/WeightBasedSampler.hpp
+++ Random/WeightBasedSampler.hpp
@@ -6,16 +6,30 @@ class WeightBasedSampler
 {
 	public:
 
-		WeightBasedSampler() : m_weightSum(0.0) 
-	{
-	}
+		WeightBasedSampler() 
+			: m_weightSum(0.0) 
+		{
+		}
 
-		void Add(double weight, Value_t value)
+		void add(double weight, Value_t value)
 		{       
 			m_weightSum += weight;
 			m_cumulativeWeights.push_back(m_weightSum);
 			m_values.push_back(value);
 			m_uniformDist = std::uniform_real_distribution<double>(0,m_cumulativeWeights.back());
+		}
+
+    void reserve(size_t reservedSize)
+		{
+			m_cumulativeWeights.reserve(reservedSize);
+			m_values.reserve(reservedSize);
+		}
+
+    void clear()
+		{
+			m_cumulativeWeights.clear();
+			m_values.clear();
+			m_weightSum = 0.0;
 		}
 
 		template <typename RandomEngine_t>
@@ -27,47 +41,47 @@ class WeightBasedSampler
 
 		/// return -1 if index cannot be found
 		template <typename T>
-		static int getIndex(T value, const std::vector<T>& weights) 
-		{
-			if ( weights.empty() || value > weights.back() )
+			static int getIndex(T value, const std::vector<T>& weights) 
 			{
-				return -1;
-			}
-
-			int left, right, middle;
-			int index = -1;
-
-			left = 0;
-			right = weights.size() - 1;
-
-			while (left <= right) 
-			{
-				middle = (left + right + 1) / 2;
-
-				if (middle == 0) 
+				if ( weights.empty() || value > weights.back() )
 				{
-					if ( value < weights[0] ) 
+					return -1;
+				}
+
+				int left, right, middle;
+				int index = -1;
+
+				left = 0;
+				right = weights.size() - 1;
+
+				while (left <= right) 
+				{
+					middle = (left + right + 1) / 2;
+
+					if (middle == 0) 
 					{
-						index = 0;
+						if ( value < weights[0] ) 
+						{
+							index = 0;
+						}
+						break;
 					}
-					break;
+					else if ( value <= weights[middle] && value > weights[middle-1] )
+					{
+						index = middle;
+						break;
+					} 
+					else if ( value > weights[middle] ) 
+					{
+						left = middle + 1;
+					} 
+					else 
+					{
+						right = middle - 1;
+					}
 				}
-				else if ( value <= weights[middle] && value > weights[middle-1] )
-				{
-					index = middle;
-					break;
-				} 
-				else if ( value > weights[middle] ) 
-				{
-					left = middle + 1;
-				} 
-				else 
-				{
-					right = middle - 1;
-				}
+				return index;
 			}
-			return index;
-		}
 
 	private:
 		std::uniform_real_distribution<double> m_uniformDist;
