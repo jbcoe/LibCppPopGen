195c851cae65d1081bba0adc9044b70379150354
Jonathan Coe <jon@jbcoe.net>
2012-02-26 12:43:49 +0000

    Tests for weight based sampler

---
+++ Random/TestWeightBasedSampler.hpp
@@ -0,0 +1,29 @@
+#include "WeightBasedSampler.hpp"
+#include <cppunit/TestSuite.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class TestWeightBasedSampler : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( TestWeightBasedSampler );
+	CPPUNIT_TEST ( testCumulativeWeightLookup ); 
+	CPPUNIT_TEST_SUITE_END();
+
+	protected:
+	void testCumulativeWeightLookup()
+	{
+		WeightBasedSampler<std::string> stringSampler;
+		stringSampler.add(2.,"Cat");
+
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(0.) == "Cat");
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(1.) == "Cat");
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(2.) == "Cat");
+
+		stringSampler.add(1.,"Pig");
+
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(0.) == "Cat");
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(1.) == "Cat");
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(2.) == "Cat");
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(2.01) == "Pig");
+		CPPUNIT_ASSERT(stringSampler.GetValueForWeight(3.) == "Pig");
+	}
+};
--- Tests/TestRunner.cpp
+++ Tests/TestRunner.cpp
@@ -3,25 +3,12 @@
 #include <cppunit/TestSuite.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/ui/text/TestRunner.h>
-
-class ExampleTestCase : public CppUnit::TestFixture
-{
-	CPPUNIT_TEST_SUITE( ExampleTestCase );
-  CPPUNIT_TEST ( testAddition ); 
-	CPPUNIT_TEST_SUITE_END();
-
-protected:
-	void testAddition()
-	{
-		CPPUNIT_ASSERT( 2+2 == 4 );
-	}
-};
-
+#include "../Random/TestWeightBasedSampler.hpp"
 
 int main (int argc, char** argv)
 {
 	CppUnit::TextUi::TestRunner runner;
-	runner.addTest( ExampleTestCase::suite() );
+	runner.addTest( TestWeightBasedSampler::suite() );
 	runner.run();
 	return 0;
 }
